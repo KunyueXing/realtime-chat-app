d4b75d6ddb79e6a2063fba8772f39b80485ca00b
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-09-11 02:09:52 -0700

    Added the feature that user can change the app's language, and support the translation of text.

---
+++ src/hooks/useLocales.js
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+import { langOptions, defaultLang } from '../config';
+
+function useLocales() {
+    const {i18n, t: translate } = useTranslation();
+
+    const handleChangeLanguage = (newLang) => {
+        i18n.changeLanguage(newLang);
+    }
+
+    return {
+        onChangeLang: handleChangeLanguage,
+        translate: (text, options) => translate(text, options),
+        langOptions,
+    };
+}
+
+export default useLocales;
