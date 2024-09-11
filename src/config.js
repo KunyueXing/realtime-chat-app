db7e07e36790f32a188bea35a5ae23ad83548bc8
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-09-11 01:34:03 -0700

    Create config.js for the whole project and setted language options, with English as default

---
+++ src/config.js
@@ -0,0 +1,31 @@
+import { enUS, zhCN, frFR, esES} from '@mui/material/locale';
+
+export const langOptions = [
+    {
+        label: 'English',
+        value: 'en',
+        systemValue: enUS,
+        icon: '/assets/icons/flags/ic_flag_en.svg',
+      },
+      {
+        label: 'French',
+        value: 'fr',
+        systemValue: frFR,
+        icon: '/assets/icons/flags/ic_flag_fr.svg',
+      },
+      {
+        label: 'Chinese',
+        value: 'cn',
+        systemValue: zhCN,
+        icon: '/assets/icons/flags/ic_flag_cn.svg',
+      },
+      {
+        label: 'Spanish',
+        value: 'es',
+        systemValue: esES,
+        icon: '/assets/icons/flags/ic_flag_es.svg',
+      },
+];
+
+// the default language is English
+export const defaultLang = langOptions[0];
