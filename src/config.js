75e449e5f8559898a4e24bfe22e381c520e078e9
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-09-12 22:40:33 -0700

    Defined the routing configuration, set up lay-loaded routes, a fall back loading screen, and redirections. Created placeholder in components and pages.

--- src/config.js
+++ src/config.js
@@ -44,3 +44,6 @@ export const defaultSettings = {
   themeColorPresets: "default",
   themeStretch: false,
 }
+
+// DEFAULT ROOT PATH
+export const DEFAULT_PATH = PATH_DASHBOARD.general.app; // as '/app'
