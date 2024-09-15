0982a52fb4490d27630ac44447f4801d4619254a
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-09-14 22:33:27 -0700

    Updated comments in src/routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,11 @@ import LoadingScreen from '../components/LoadingScreen'
 import { DEFAULT_PATH } from '../config'
 import React from 'react'
 
+/*
+  Suspense: This component allows for showing a fallback (loading screen) while a lazy-loaded component is being loaded.
+  lazy: Used to dynamically import components, which enables code-splitting. This helps improve performance by loading 
+  parts of the application only when they are needed (on demand).
+*/
 const Loadable = (Component) => (props) => {
   return (
     <Suspense fallback={<LoadingScreen />}>
@@ -17,6 +22,10 @@ const Loadable = (Component) => (props) => {
 
 export default function Router() {
   return useRoutes([
+    /*
+      The main route has the path '/', and its element is <DashboardLayout /> -- any child routes in side this path will
+      render inside the Dashboardlayout.
+    */
     {
       path: '/',
       element: <DashboardLayout />,
@@ -25,6 +34,7 @@ export default function Router() {
         { path: 'app', element: <GeneralApp /> },
 
         { path: '404', element: <Page404 /> },
+        // When users visit a non-existing path, it will be redirected to '/404'path
         { path: '*', element: <Navigate to='/404' replace /> }
       ]
     },
@@ -32,6 +42,10 @@ export default function Router() {
   ])
 }
 
+/*
+  lazy(() => import(...)): Dynamically imports the GeneralApp / Page404 components from their respective files. This 
+  splits these components into separate chunks -- they will only be loaded when needed (on route change).
+*/
 const GeneralApp = Loadable(lazy(() => import('../pages/dashboard/GeneralApp')))
 
 const Page404 = Loadable(lazy(() => import('../pages/Page404')))
--- src/routes/paths.js
+++ src/routes/paths.js
@@ -4,6 +4,11 @@ function path(root, sublink) {
 
 const ROOTS_DASHBOARD = '/'
 
+/*
+  Instead of hardcoding strings throughout the application, import PATH_DASHBOARD and use it to reference paths consistently.
+  PATH_DASHBOARD.general.app refer to the '/app' route.
+  To improve the maintainability, especially when paths need to be changed in the future.
+*/
 export const PATH_DASHBOARD = {
   root: ROOTS_DASHBOARD,
   general: {
