b6b9620435cadd96663a24d2cd1f899d5200edca
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-09-19 01:19:00 -0700

    Started to create styled chat input in the chat footer.

--- src/components/Chat/Footer.js
+++ src/components/Chat/Footer.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Stack, TextField } from '@mui/material'
+
+const StyledInput = styled(TextField)(({ theme }) => ({
+  '& .MuiInputBase-input': {
+    paddingTop: '12px !important',
+    paddingBottom: '12px !important'
+  }
+}))
+
+const ChatInput = () => {
+  return (
+    <StyledInput>
+
+    </StyledInput>
+  )
+}
 
 const Footer = () => {
   return (
-    <>
-      <Box p={2} sx={{ width: '100%', height: 100, bgcolor: '#F8FAFF', boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)' }}></Box>
-    </>
+    <Box sx={{ position: 'relative', bgcolor: 'transparent !important' }}>
+      <Box
+        p={2}
+        sx={{
+          width: '100%',
+          height: 100,
+          bgcolor: '#F8FAFF',
+          boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)'
+        }}
+      >
+        <Stack direction='row' sx={{ alignItems: 'center' }} spacing={3}>
+          <Stack sx={{ width: '100%' }}>
+            <Box>{/* emoji */}</Box>
+          </Stack>
+        </Stack>
+      </Box>
+    </Box>
   )
 }
 
