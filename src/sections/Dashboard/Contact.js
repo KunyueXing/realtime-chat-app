df2bbfa0acc3f4adeff98dd27b1ae56c43455431
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-10-04 00:14:35 -0700

    feat: [UI] Continued building the UI of contact page, including person bio, contact info, media and links of the chat, starred messages, mute nofitications switch.

--- src/sections/Dashboard/Contact.js
+++ src/sections/Dashboard/Contact.js
@@ -1,9 +1,10 @@
-import { Box, IconButton, Stack, Typography } from '@mui/material'
+import { Avatar, Box, Button, Divider, IconButton, Stack, Switch, Typography } from '@mui/material'
 import React from 'react'
-import { XCircle } from 'phosphor-react'
+import { Bell, CaretRight, Phone, Star, VideoCamera, XCircle } from 'phosphor-react'
 import { useTheme } from '@mui/material/styles'
 import { useDispatch } from 'react-redux'
 import { ToggleSidebar } from '../../redux/slices/app'
+import { faker } from '@faker-js/faker'
 
 const Contact = () => {
   const theme = useTheme()
@@ -38,6 +39,82 @@ const Contact = () => {
           </Stack>
         </Box>
         {/* Body */}
+        <Stack sx={{ height: '100%', position: 'relative', flexGrow: 1, overflow: 'scroll' }} p={3} spacing={3}>
+          <Stack sx={{ alignItems: 'center' }} direction='row' spacing={2}>
+            <Avatar sx={{ height: 64, width: 64 }} src={faker.image.avatar()} alt={faker.person.firstName()} />
+            <Stack spacing={0.5}>
+              <Typography variant='article' fontWeight={600}>
+                {faker.person.fullName()}
+              </Typography>
+              <Typography variant='article' fontWeight={500}>
+                {faker.phone.number({ style: 'national' })}
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction='row' sx={{ alignItems: 'center', justifyContent: 'space-evenly' }}>
+            <Stack sx={{ alignItems: 'center' }} spacing={1}>
+              <IconButton>
+                <Phone />
+              </IconButton>
+              <Typography variant='overline'>Voice</Typography>
+            </Stack>
+            <Stack sx={{ alignItems: 'center' }} spacing={1}>
+              <IconButton>
+                <VideoCamera />
+              </IconButton>
+              <Typography variant='overline'>Video</Typography>
+            </Stack>
+          </Stack>
+          <Divider />
+          <Stack spacing={0.5}>
+            <Typography variant='article' fontWeight={600}>
+              About
+            </Typography>
+            <Typography variant='body2' fontWeight={500}>
+              {faker.person.bio()}
+            </Typography>
+          </Stack>
+          <Divider />
+          <Stack direction={'row'} sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
+            <Typography variant='subtitle2'>Media, Links and Docs</Typography>
+            <Button endIcon={<CaretRight />}>401</Button>
+          </Stack>
+          <Stack direction={'row'} sx={{ alignItems: 'center' }} spacing={2}>
+            {[1, 2, 3].map((ele, index) => (
+              <Box key={`contactMedia-${index}`} width={'90px'} height={'90px'}>
+                <img
+                  src={faker.image.url('cat')}
+                  alt={faker.person.fullName()}
+                  style={{
+                    width: '100%', // means the image will take full width of the Box.
+                    height: '100%',
+                    objectFit: 'cover', // ensures the image fits within the box without distortion
+                    borderRadius: '1'
+                  }}
+                />
+              </Box>
+            ))}
+          </Stack>
+          <Divider />
+          <Stack direction={'row'} sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
+            <Stack direction={'row'} sx={{ alignItems: 'center' }} spacing={2}>
+              <Star size={21} />
+              <Typography variant='subtitle2'>Starred Messages</Typography>
+            </Stack>
+            <IconButton>
+              <CaretRight />
+            </IconButton>
+          </Stack>
+          <Divider />
+          <Stack direction={'row'} sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
+            <Stack direction={'row'} sx={{ alignItems: 'center' }} spacing={2}>
+              <Bell size={21} />
+              <Typography variant='subtitle2'>Mute Notifications</Typography>
+            </Stack>
+            <Switch />
+          </Stack>
+          <Divider />
+        </Stack>
       </Stack>
     </Box>
   )
