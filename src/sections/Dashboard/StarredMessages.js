3348ebd2293d29253f891f9540f8622c7efd3401
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-10-06 22:32:26 -0700

    Feat: [UI] Finished the starred messages page on sidebar.

--- src/sections/Dashboard/StarredMessages.js
+++ src/sections/Dashboard/StarredMessages.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Box, IconButton, Stack, Typography } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { ArrowLeft } from 'phosphor-react'
+import { useDispatch } from 'react-redux'
+import { UpdateSidebarType } from '../../redux/slices/app'
+import { sidebarPageMappings } from '../../utils/constants'
+import ChatComponent from '../../pages/dashboard/Conversation'
 
 const StarredMessages = () => {
-  return <div>StarredMessages</div>
+  const theme = useTheme()
+  const dispatch = useDispatch()
+
+  return (
+    <Box sx={{ width: 320, maxHeight: '100vh' }}>
+      <Stack sx={{ height: '100%', width: 326 }}>
+        <Box
+          sx={{
+            boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
+            width: '100%',
+            bgcolor: theme.palette.mode === 'light' ? '#F8FAFF' : theme.palette.background
+          }}
+        >
+          <Stack sx={{ height: '100%', p: 2, alignItems: 'center' }} direction={'row'} spacing={3}>
+            <IconButton
+              onClick={() => {
+                dispatch(UpdateSidebarType(sidebarPageMappings[0]))
+              }}
+            >
+              <ArrowLeft />
+            </IconButton>
+            <Typography variant='subtitle2'>Starred Messages</Typography>
+          </Stack>
+        </Box>
+        <Stack sx={{ height: '100%', position: 'relative', flexGrow: 1, overflow: 'scroll' }} spacing={3}>
+          <ChatComponent />
+        </Stack>
+      </Stack>
+    </Box>
+  )
 }
 
 export { StarredMessages }
