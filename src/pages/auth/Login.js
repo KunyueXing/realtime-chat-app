fcc6e09b91ac27a8d9527a5694d52a6b21728c41
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-10-14 01:08:13 -0700

    Feat: [UI] Implemented the UI of login page and created the register page.

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,22 @@
+import { Stack, Typography, Link } from '@mui/material'
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <>
+      <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
+        <Typography variant='h4'>Login to ChatElephant</Typography>
+        <Stack direction={'row'} spacing={0.5}>
+          <Typography variant='body2'>New user?</Typography>
+          <Link component={RouterLink} variant='subtitle2' to={'/auth/register'}>
+            Create an account
+          </Link>
+        </Stack>
+      </Stack>
+      {/* login form */}
+    </>
+  )
 }
 
 export default Login
