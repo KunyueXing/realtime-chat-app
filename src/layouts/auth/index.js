2433eb799e29934ea33ef17254de69d2fb18182a
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2024-10-14 00:33:08 -0700

    Feat: [UI] Finished created the auth layout.

--- src/layouts/auth/index.js
+++ src/layouts/auth/index.js
@@ -1,11 +1,19 @@
+import { Container, Stack } from '@mui/material'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import LoginLogo from '../../assets/Images/elephant.ico'
 
 const AuthLayout = () => {
   return (
     <>
-      <div>authlayout</div>
-      <Outlet />
+      <Container sx={{ mt: 5 }} maxWidth='sm'>
+        <Stack spacing={5}>
+          <Stack sx={{ width: '100%', alignItems: 'center' }} direction={'column'}>
+            <img style={{ height: 240, width: 240 }} src={LoginLogo} alt='LoginLogo' />
+          </Stack>
+          <Outlet />
+        </Stack>
+      </Container>
     </>
   )
 }
