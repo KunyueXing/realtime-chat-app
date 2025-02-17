a513dabd200f5b6e364fa72a8630d4f5e7476262
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-02-16 23:32:46 -0800

    Feat: Added the navigation that when clicks the profile in the avatar menu, it routes to the Profile page.

--- src/layouts/dashboard/ProfileMenu.js
+++ src/layouts/dashboard/ProfileMenu.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Profile_Menu } from '../../data'
 import { Box, Stack, Avatar, Menu, MenuItem, Fade } from '@mui/material'
 import { faker } from '@faker-js/faker'
+import { useNavigate } from 'react-router-dom'
 
 const ProfileMenu = () => {
   const [anchorEl, setAnchorEl] = useState(null)
   const openMenu = Boolean(anchorEl)
+  const [selectedMenu, setSelectedMenu] = useState(null)
+  const navigate = useNavigate()
+
+  // Handle click event on the avatar. When clicked, it opens the menu. Clicking again or outside the menu closes it.
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget)
   }
   const handleClose = () => {
     setAnchorEl(null)
   }
+
+  // Handle click event on the menu items. When clicked, it sets the selected menu index and closes the menu.
+  const handleMenuItemClick = (index) => {
+    // console.log(`Menu item ${index} clicked`)
+    setSelectedMenu(index)
+    handleClose()
+  }
+
+  useEffect(() => {
+    // console.log(`Selected menu: ${selectedMenu}`)
+    if (selectedMenu !== null && selectedMenu === 0) {
+      // console.log('Navigating to /profile')
+      navigate('/profile')
+    }
+  }, [selectedMenu, navigate])
 
   return (
     <>
@@ -39,7 +59,7 @@ const ProfileMenu = () => {
         <Box p={1}>
           <Stack spacing={1}>
             {Profile_Menu.map((ele, index) => (
-              <MenuItem onClick={handleClose} key={`profile-menu-${index}`}>
+              <MenuItem onClick={() => handleMenuItemClick(index)} key={`profile-menu-${index}`}>
                 <Stack direction='row' sx={{ width: 100, alignItems: 'center', justifyContent: 'space-between' }}>
                   <span>{ele.title}</span>
                   {ele.icon}
