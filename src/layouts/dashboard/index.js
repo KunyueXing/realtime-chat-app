b6b508dc2777775a25143ac31e9c30e3f885d6ac
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-04-22 15:20:59 -0700

    feat: added detection of whether is loggedin in layouts

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
 import { Stack } from '@mui/material'
 import SideNav from './SideNav'
+import { useSelector } from 'react-redux'
 
 const DashboardLayout = () => {
+  const { isLoggedIn } = useSelector((state) => state.auth)
+
+  if (!isLoggedIn) {
+    return <Navigate to={'/auth/login'} />
+  }
+
   return (
     <>
       <Stack direction='row'>
--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const MainLayout = () => {
+  const { isLoggedIn } = useSelector((state) => state.auth)
+
+  if (!isLoggedIn) {
+    return <Navigate to={'/auth/login'} />
+  }
+
   return (
     <>
       <div>Main Layout</div>
