7b668aa20846a57c3b9e6a3a2b11d28d42eeb40d
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-04-25 02:37:04 -0700

    feat: added a Snackbar in the app redux slice and added it to App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,55 @@ import Router from './routes'
 import React from 'react'
 // theme
 import ThemeProvider from './theme'
+import { Snackbar } from '@mui/material'
+import MuiAlert from '@mui/material/Alert'
+import { useDispatch, useSelector } from 'react-redux'
+import { CloseSnackBar } from './redux/slices/app'
 // components
 
+const vertical = 'bottom'
+const horizontal = 'center'
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
+})
+
 function App() {
+  const dispatch = useDispatch()
+  const { open, message, severity } = useSelector((state) => state.app.snackbar)
+
   return (
-    <ThemeProvider>
-      {' '}
-      <Router />{' '}
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        {' '}
+        <Router />{' '}
+      </ThemeProvider>
+
+      {message && open ? (
+        <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          autoHideDuration={4000}
+          key={vertical + horizontal}
+          onClose={() => {
+            console.log('This is clicked')
+            dispatch(CloseSnackBar())
+          }}
+        >
+          <Alert
+            onClose={() => {
+              console.log('This is clicked')
+              dispatch(CloseSnackBar())
+            }}
+            severity={severity}
+            sx={{ width: '100%' }}
+          >
+            {message}
+          </Alert>
+        </Snackbar>
+      ) : (
+        <></>
+      )}
+    </>
   )
 }
 
--- src/redux/slices/app.js
+++ src/redux/slices/app.js
@@ -43,6 +43,18 @@ const slice = createSlice({
     },
     updateSidebarType(state, action) {
       state.sideBar.type = action.payload.type
+    },
+    openSnackBar(state, action) {
+      console.log(action.payload)
+      state.snackbar.open = true
+      state.snackbar.severity = action.payload.severity
+      state.snackbar.message = action.payload.message
+    },
+    closeSnackBar(state) {
+      console.log('This is getting executed')
+      state.snackbar.open = false
+      state.snackbar.severity = null
+      state.snackbar.message = null
     }
   }
 })
@@ -64,5 +76,21 @@ export function ToggleSidebar() {
 export function UpdateSidebarType(type) {
   return async (dispatch, getState) => {
     dispatch(slice.actions.updateSidebarType({ type }))
+  }
+}
+
+export function OpenSnackBar(severity, message) {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.openSnackBar({ severity, message }))
+
+    setTimeout(() => {
+      dispatch(slice.actions.closeSnackBar())
+    }, 4000)
+  }
+}
+
+export function CloseSnackBar() {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.closeSnackBar())
   }
 }
