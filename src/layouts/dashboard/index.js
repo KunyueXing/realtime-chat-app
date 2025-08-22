71873b45a1c861dffa98f886de3dea77d1aa8a26
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-08-22 01:16:09 -0700

    refactor: Added socket handlers and friend socket handlers and incorperate socket handlers in dashboard layout

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -4,6 +4,7 @@ import { Stack } from '@mui/material'
 import SideNav from './SideNav'
 import { useSelector } from 'react-redux'
 import useDashboardSocketHandlers from '../../hooks/socket-events/useDashboardSocketHandlers'
+import { useSocketHandlers } from '../../hooks/socketEvents/useSocketHandlers'
 
 const DashboardLayout = () => {
   const { isLoggedIn } = useSelector((state) => state.auth)
@@ -11,7 +12,8 @@ const DashboardLayout = () => {
   console.log('layout/dashboard user_id:', user_id)
 
   // Handle socket events of audio/video calls, new messages, start chat, and friend requests
-  useDashboardSocketHandlers({ user_id, isLoggedIn })
+  // useDashboardSocketHandlers({ user_id, isLoggedIn })
+  useSocketHandlers({ userId: user_id, isLoggedIn })
 
   if (!isLoggedIn) {
     return <Navigate to={'/auth/login'} />
